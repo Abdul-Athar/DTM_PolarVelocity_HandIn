26f2621bd24bf234fe3bf44144361c7869b346ce
Abdul-Athar <21443@wc.school.nz>
2023-08-17 10:03:33 +1200

    movement failed

---
+++ PolarVelocity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    public float turnSpeed = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get movement values from keyboard
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the object
+        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput);
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput);
+        transform.Rotate(Vector3.up * horizontalInput * turnSpeed * Time.deltaTime);
+
+    }
+}
