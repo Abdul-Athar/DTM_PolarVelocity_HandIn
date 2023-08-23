0d552bff8362aac2d768c1aa16960bef6fcd25d7
Abdul-Athar <21443@wc.school.nz>
2023-08-24 10:12:06 +1200

    Movement failure #100000

--- PolarVelocity/Assets/Scripts/PlayerController.cs
+++ PolarVelocity/Assets/Scripts/PlayerController.cs
@@ -7,23 +7,24 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float verticalInput;
     public float turnSpeed = 30;
+    Rigidbody player_rb;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        player_rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         //get movement values from keyboard
-        horizontalInput = Input.GetAxis("Horizontal");
-        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = 2*(Input.GetAxis("Horizontal"));
+        verticalInput = 2*(Input.GetAxis("Vertical"));
 
         // move the object
-        transform.Translate(Vector3.forward * Time.deltaTime * verticalInput);
-        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput);
+        player_rb.MovePosition(Vector3.forward * Time.deltaTime * verticalInput);
+        player_rb.MovePosition(Vector3.right * Time.deltaTime * horizontalInput);
         transform.Rotate(Vector3.up * horizontalInput * turnSpeed * Time.deltaTime);
 
     }
